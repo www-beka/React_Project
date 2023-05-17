fa7c7244d63ee11dcbb8ba13e5747f0fa07a6479
Bekzod <bekmuradovbekzod955@gmail.com>
2023-05-17 22:53:57 +0500

    small changes

--- crypter-app/src/Components/Home/Menu Nav/Left_and_right_Bars/LeftNavBar.jsx
+++ crypter-app/src/Components/Home/Menu Nav/Left_and_right_Bars/LeftNavBar.jsx
@@ -1,13 +1,16 @@
 import Logo from '../../../../assets/logo.svg'
-
+import { Outlet, Link } from "react-router-dom";
+import scss from '../sass/header.scss';    
 const LeftNavBar = () => {
     return(
        <>
           <div className="navbar-left">
             <div className="navbar-logo">
                 <div>
-                    <img src={Logo} /> 
-                    <span>crypter</span>    
+                    <Link to='/'>
+                        <img src={Logo} />
+                        <span>crypter</span>    
+                    </Link> 
                 </div>
             </div>  
           </div>
