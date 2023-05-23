7f58c2a0b07656a23431cb7945b7795d04640e85
Bekzod <bekmuradovbekzod955@gmail.com>
2023-05-23 19:01:34 +0500

    small changes

--- crypter-app/src/Components/Discover/PopularCollections/Cards/index.jsx
+++ crypter-app/src/Components/Discover/PopularCollections/Cards/index.jsx
@@ -7,23 +7,21 @@ const PopularCollection_Cards = (props) => {
             <div className="PopularCollection_Cards">
                 <div className="cards_images">
                     <div className="hero_images">
-                        <img src={props.heroImg} alt="" />
+                        <img src={props.heroImg} alt=""/>
                     </div>
                     <div className='other_images'>
                         <img src={props.otherImg_1} alt="" />
-
                         <img src={props.otherImg_2} alt="" />
                         <div className="counter">
                             <span>{props.otherImg_3}</span>
                         </div>
                     </div>
-
                 </div>
                 <div className="cards_desk">
                     <div className="cards_left_desk">
                         <p>{props.title}</p>
                         <div className="user">
-                            <img src={props.userAvatar} alt="" />
+                            <img src={props.userAvatar} alt="" width={32}/>
                         </div>
                         <b>{props.userName}</b>
                     </div>
