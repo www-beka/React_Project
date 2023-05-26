8cf0da929f979d283c7069a149976f2916ae4fd3
Bekzod <bekmuradovbekzod955@gmail.com>
2023-05-26 15:56:22 +0500

    small changes for 404 page

--- crypter-app/src/Components/NoPage.jsx
+++ crypter-app/src/Components/NoPage.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import No_Page from '../assets/images/no_page.jpg';
+
 const NoPage = () => {
     return(
         <>
             <div className="no-page">
+                <p>Page not found</p>
                 <h1>404</h1>
+                <p>We couldn't page you're looking for</p>
+                <button>
+                    <Link to='/'>
+                        Back
+                    </Link>
+                </button>
             </div>
         </>
     ) 
